6b97112ab8b362d27f7976815eb0d35da5249099
Michael McLoughlin <mmcloughlin@gmail.com>
2017-09-22 15:18:22 -0700

    doc

--- pprof.go
+++ pprof.go
@@ -6,10 +6,12 @@ import (
 	"net/http/pprof"
 )
 
+// init disables default handlers registered by importing net/http/pprof.
 func init() {
 	http.DefaultServeMux = http.NewServeMux()
 }
 
+// Handle adds standard pprof handlers to mux.
 func Handle(mux *http.ServeMux) {
 	mux.HandleFunc("/debug/pprof/", pprof.Index)
 	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
@@ -18,12 +20,14 @@ func Handle(mux *http.ServeMux) {
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 }
 
+// NewServeMux builds a ServeMux and populates it with standard pprof handlers.
 func NewServeMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	Handle(mux)
 	return mux
 }
 
+// NewServer constructs a server at addr with the standard pprof handlers.
 func NewServer(addr string) *http.Server {
 	return &http.Server{
 		Addr:    addr,
@@ -31,10 +35,12 @@ func NewServer(addr string) *http.Server {
 	}
 }
 
+// ListenAndServe starts a server at addr with standard pprof handlers.
 func ListenAndServe(addr string) error {
 	return NewServer(addr).ListenAndServe()
 }
 
+// Launch a standard pprof server at addr.
 func Launch(addr string) {
 	go func() {
 		log.Fatal(ListenAndServe(addr))
